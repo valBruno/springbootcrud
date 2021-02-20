3204411aaa473e6e235faa54d8c538167f6450c9
bruno <bruno.val@hepta.com.br>
2021-02-20 20:03:12 -0300

    token

---
+++ src/main/java/com/empresa/springbootcrud/config/security/AutenticacaoViaTokenFilter.java
@@ -0,0 +1,54 @@
+package com.empresa.springbootcrud.config.security;
+
+import com.empresa.springbootcrud.model.Usuario;
+import com.empresa.springbootcrud.repository.UsuarioRepo;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
+	
+	private TokenService tokenService;
+	private UsuarioRepo repo;
+
+	public AutenticacaoViaTokenFilter(TokenService tokenService, UsuarioRepo repo) {
+		this.tokenService = tokenService;
+		this.repo = repo;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		String token = recuperarToken(request);
+		boolean valido = tokenService.isTokenValido(token);
+		if (valido) {
+			autenticarCliente(token);
+		}
+		
+		filterChain.doFilter(request, response);
+	}
+
+	private void autenticarCliente(String token) {
+		Long idUsuario = tokenService.getIdUsuario(token);
+		Usuario usuario = repo.findById(idUsuario).get();
+		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+	}
+
+	private String recuperarToken(HttpServletRequest request) {
+		String token = request.getHeader("Authorization");
+		if (token == null || token.isEmpty() || !token.startsWith("Bearer ")) {
+			return null;
+		}
+		
+		return token.substring(7, token.length());
+	}
+
+}
